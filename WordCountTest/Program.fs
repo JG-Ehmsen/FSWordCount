6d4d7ef954a8442eab3580c1056d44f17e88074e
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2020-01-06 17:30:21 +0100

    Init

---
+++ WordCountTest/Program.fs
@@ -0,0 +1,15 @@
+﻿open System.IO
+
+let readFile = File.ReadAllText(@"PATH TO TEXT")
+
+let splitWords (string: string) =
+  string.Split([|','; '.'; ' '|])
+  |> Array.map (fun s -> s.Trim())
+  |> Array.filter(fun x -> not(x.Equals("")))
+
+let count words =
+  words
+  |> Seq.groupBy id
+  |> Seq.map (fun (w, ws) -> (w, Seq.length ws))
+  |> Seq.sortBy (snd >> (~-))
+  |> Seq.toList
